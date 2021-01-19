21b4cd679847a0b1f417cd0fca0ba5fc65eede5d
PeterMaxwell <peter@allicient.co.uk>
2021-01-19 19:58:55 +0100

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.10

---
+++ src/MPCCModelManager.jl
@@ -0,0 +1,5 @@
+module MPCCModelManager
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using MPCCModelManager
+using Test
+
+@testset "MPCCModelManager.jl" begin
+    # Write your tests here.
+end
